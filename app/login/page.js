dcd5adcd84d2d5968a83ba78f14c074949afcf1b
alexi <108108603+itsalexi@users.noreply.github.com>
2024-10-25 17:17:47 +0800

    remove typescript

---
+++ app/login/page.js
@@ -0,0 +1,68 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { auth, googleAuthProvider } from '@/lib/firebase';
+import { UserContext } from '@/lib/context';
+import { useContext } from 'react';
+
+export default function Login() {
+    const { user } = useContext(UserContext);
+
+    const signInWithGoogle = async () => {
+        try {
+            await auth.signInWithPopup(googleAuthProvider);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="login-page">
+            {user ? (
+                <div>You are logged in</div>
+            ) : (
+                <div className="login-box">
+                    <div className="login-top">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            Create an account
+                        </h2>
+                        <p className="text-sm text-gray-500">
+                            You can only login with your Ateneo account
+                            (*.ateneo.edu)
+                        </p>
+                    </div>
+                    <hr />
+
+                    <Button className="mt-3" onClick={signInWithGoogle}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="52 42 88 66"
+                        >
+                            <path
+                                fill="#4285f4"
+                                d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6"
+                            />
+                            <path
+                                fill="#34a853"
+                                d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15"
+                            />
+                            <path
+                                fill="#fbbc04"
+                                d="M120 48v26l20-15v-8c0-7.42-8.47-11.65-14.4-7.2"
+                            />
+                            <path
+                                fill="#ea4335"
+                                d="M72 74V48l24 18 24-18v26L96 92"
+                            />
+                            <path
+                                fill="#c5221f"
+                                d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2"
+                            />
+                        </svg>
+                        Login with your Gmail
+                    </Button>
+                </div>
+            )}
+        </div>
+    );
+}
