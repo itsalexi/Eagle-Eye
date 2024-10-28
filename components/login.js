04855c413ee8a579d6db676cbfb860894852f3b1
itsalexi <108108603+itsalexi@users.noreply.github.com>
2024-10-28 15:38:53 +0800

    fixed the issue :)

--- components/login.js
+++ components/login.js
@@ -21,7 +21,7 @@ export default function LoginBox() {
                 router.push('/');
             }
         } catch (err) {
-            setError(err);
+            setError(err.toString());
         }
     };
 
